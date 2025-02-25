857b0affc20ea3f4cc6fd232a8031672552e1b87
rajaram2003 <rajarambiswal123@gmail.com>
2025-02-25 22:28:22 +0530

    backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -50,13 +50,31 @@ export const signup = async (req,res) => {
 };
 
 export const login = async (req,res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
   try {
-    const user = await User.findOne({email})
+    const user = await User.findOne({email});
+
+    if(!user) {
+    return res.status(400).json({message:"Invalid credentials"});
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if(!isPasswordCorrect) {
+      return res.status(400).json({message:"Invalid credentials"});
+    }
+
+    generateToken(user._id)
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
     
-  } catch (error) {
-    return res.status(400).json({message:"Invalid credentials"})
-    
+  } catch (error) { 
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 

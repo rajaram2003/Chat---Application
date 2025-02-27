607aa5f349c94a6076f8ec6d480a29640b5a15db
rajaram2003 <rajarambiswal123@gmail.com>
2025-02-27 21:50:32 +0530

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
@@ -13,9 +14,10 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
-  connectDB()
-})
+  connectDB();
+});

e2799b53bb187cd1061dedbddb6b53d5dfedeb80
rajaram2003 <rajarambiswal123@gmail.com>
2025-03-03 19:00:45 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,18 @@ import LoginPage from "./pages/LoginPage"
 import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import SignUpPage from "./pages/SignUpPage"
-
+import { useAuthStore } from "./store/useAuthStore"
+import { useEffect } from "react"
 
 const App = () => {
+  const {authUser,checkAuth} = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth]);
+
+  console.log({authUser});
+
   return (
     <div className="">
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 
 
 const Navbar = () => {
+
   return (
     <div>
       

524fbc0731cceb3f65926904f071df6b689b0596
rajaram2003 <rajarambiswal123@gmail.com>
2025-03-03 17:50:47 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
+import SignUpPage from "./pages/SignUpPage"
 
 
 const App = () => {
   return (
-    <div className="text-red-500">
-      Hello world
+    <div className="">
+
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/settings" element={<SettingsPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+      </Routes>
+
     </div>
   )
 }

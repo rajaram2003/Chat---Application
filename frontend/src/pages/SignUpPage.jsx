f96b0844fda71220f470d84dc6ea39bf30d9599f
rajaram2003 <rajarambiswal123@gmail.com>
2025-03-04 23:07:20 +0530

    frontend

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -9,9 +9,9 @@ const SignUpPage = () => {
     password: "",
   });
 
-  const {signup, isSignUp} = useAuthStore();
+  const { signup, isSignUp } = useAuthStore();
 
-  const validateForm = () => {}
+  const validateForm = () => { }
   const handleSubmit = (e) => {
     e.preventDefault()
   };
@@ -19,13 +19,27 @@ const SignUpPage = () => {
   return (
     <div className="min-h-screen grid lg:grid-cols-2">
 
-     {/* Left Side */}
-    <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+      {/* Left Side */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
 
-      <div></div>
+          {/* Logo */}
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                <messageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2"></h1>
+              <p className="text-base-content/60">Get started with your free account</p>
+            </div>
+          </div>
 
+          <form onSubmit={handleSubmit} className="space-y-6">
+            
+          </form>
+        </div>
+      </div>
     </div>
-  </div>
   )
 };
 

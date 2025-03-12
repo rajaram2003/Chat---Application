153d9f63c75b7ec8e67003c80aa076ea885f7f97
rajaram2003 <rajarambiswal123@gmail.com>
2025-03-12 16:20:18 +0530

    frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -28,34 +28,33 @@ export const useAuthStore = create((set, get) => ({
     }
   },
 
-  signup: async (data) => {
-    set({ isSigningUp: true });
-    try {
-      const res = await axiosInstance.post("/auth/signup", data);
-      set({ authUser: res.data });
-      toast.success("Account created successfully");
-      get().connectSocket();
-    } catch (error) {
-      toast.error(error.response.data.message);
-    } finally {
-      set({ isSigningUp: false });
-    }
-  },
+    signup: async (data) => {
+      set({ isSigningUp: true });
+      try {
+        const res = await axiosInstance.post("/auth/signup", data);
+        set({ authUser: res.data });
+        toast.success("Account created successfully");
+        get().connectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isSigningUp: false });
+      }
+    },
 
   login: async (data) => {
     set({ isLoggingIn: true });
+    
     try {
       const res = await axiosInstance.post("/auth/login", data);
-      set({ authUser: res.data });
+      set({ authUser: res.data, isLoggingIn: false });
       toast.success("Logged in successfully");
-
       get().connectSocket();
     } catch (error) {
-      toast.error(error.response.data.message);
-    } finally {
       set({ isLoggingIn: false });
+      toast.error(error.response?.data?.message || "Login failed.");
     }
-  },
+  },  
 
   logout: async () => {
     try {
